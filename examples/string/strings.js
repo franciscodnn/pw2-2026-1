24b5d81e4837c25cace23c390bf2cc2c59f3a1e9
Francisco Dantas <dantas.nobre@ifpb.edu.br>
2026-03-23 17:05:03 -0300

    docs: add lecture notes about string

---
+++ examples/string/strings.js
@@ -0,0 +1,36 @@
+const nome = "Francisco";
+
+/*
+console.log("Nome: " + nome + ", qtd de caracteres: " + nome.length);
+console.log(`Nome: ${nome}, qtd de caracteres: ${nome.length}`);
+
+console.log(
+    "<img src=\"./img/ifpb.png\" />"
+)
+*/
+
+const copyright = '\u00a9';
+// console.log( `Copyright ${ copyright }`);
+
+/*
+console.log( "Francisco".includes("a") );
+console.log( "lorem ipsum ".repeat(5) );
+console.log( "Francisco".startsWith("a") );
+console.log( "Francisco".endsWith("Sco") );
+
+console.log( ['ls', 'pw', 'dir'][0].includes('l') );
+
+console.log('lorem ipsum'.substr(6, 3))
+
+console.log('lorem ipsum'.substring(5, 2));
+
+console.log('lorem ipsum'.slice(2, -1));
+
+console.log(
+    'lorem ipsum'.split(' ')[0].split('').reverse().join('') 
+);
+*/
+
+// console.log('lorem lorem lorem ipsum'.replaceAll('lorem', 'LOREM'));
+
+console.log('lorem     ipsum'.replace(/\s+/g, ' '));
